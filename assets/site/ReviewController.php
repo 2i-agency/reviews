7534147954f5efac8de2cc1620a846b961c23ffc
aboldyrev <boldyrev.a.s@studio2i.ru>
2016-10-24 18:05:34 +0300

    Добавил язык в метод reviews контроллера ReviewController

--- assets/site/ReviewController.php
+++ assets/site/ReviewController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use Chunker\Base\Models\Language;
 use Chunker\Reviews\Models\Review;
 
 class ReviewController extends Controller
@@ -10,7 +11,7 @@ class ReviewController extends Controller
 	/*
 	 *  Отзывы
 	 */
-	public function reviews() {
+	public function reviews(Language $language) {
 		$reviews = Review::where('published_at', '<>', '')->get();
 		return view('site.reviews', compact('reviews'));
 	}
